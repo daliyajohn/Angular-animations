c48df9fa01806aa8dbc3bc47fe00a902844423a3
daliyajohn75@gmail.com <daliyajohn75@gmail.com>
2018-01-26 00:06:45 +0530

    list without animation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,8 +9,19 @@ import { RouterModule, Routes, Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-    constructor(private router: Router) { }
+  items = [];
+    constructor(private router: Router) { 
+      this.items = ['i am daliya', 'i am seeniya', 'i am viji'];
+    }
+
     btnClick() {
       this.router.navigateByUrl('/animation');
+    }
+    addItem() {
+      this.items.push('i am vincy');
+    }
+  
+    removeItem() {
+      this.items.pop();
     }
 }
